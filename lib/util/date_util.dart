41684569dc44f252785da26e6cfb53c3d2da9165
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2021-01-13 17:49:41 +0800

    specify data type of variable

--- lib/util/date_util.dart
+++ lib/util/date_util.dart
@@ -1,11 +1,11 @@
-extension DateUtil on DateTime {
+ extension DateUtil on DateTime {
   static bool isValid(String original) {
     try {
-      final dateTime = DateTime.parse(original);
-      final year = dateTime.year.toString().padLeft(4, '0');
-      final month = dateTime.month.toString().padLeft(2, '0');
-      final day = dateTime.day.toString().padLeft(2, '0');
-      final formatted = '$year-$month-$day';
+      final DateTime dateTime = DateTime.parse(original);
+      final String year = dateTime.year.toString().padLeft(4, '0');
+      final String month = dateTime.month.toString().padLeft(2, '0');
+      final String day = dateTime.day.toString().padLeft(2, '0');
+      final String formatted = '$year-$month-$day';
       return original == formatted;
     } catch (e) {
       return false;
